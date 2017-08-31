f6ae934035be4112d3a03654c2621c228401c270
universalmind <universalmind.candy@gmail.com>
2017-08-31 13:17:50 -0500

    changed from util to bluebird

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
-const {promisify} = require('util');
+const {promisify} = require('bluebird');
+// const {promisify} = require('util');
 const readFileAsync = promisify(fs.readFile);
 const writeFileAsync = promisify(fs.writeFile);
 const appendFileAsync = promisify(fs.appendFile);
